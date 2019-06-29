9297d01f710a08e7642b4c1fd47cba7e4e2341e2
Marco Marinello <marco.marinello@school.rainerum.it>
2019-06-29 09:38:13 +0200

    Set upload handler
    Signed-off-by: Marco Marinello <marco.marinello@school.rainerum.it>

--- plic_pipeline_gui/plic_pipeline_gui/settings.py
+++ plic_pipeline_gui/plic_pipeline_gui/settings.py
@@ -114,3 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Set upload handlers
+FILE_UPLOAD_HANDLERS = [
+    'django.core.files.uploadhandler.TemporaryFileUploadHandler',
+]
