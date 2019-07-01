9f86d0c7b09784e69c4b8501cb7570be54a034f8
Marco Marinello <marco.marinello@school.rainerum.it>
2019-07-01 12:14:40 +0200

    Start implementing artiacts

--- plic_pipeline_gui/plic_pipeline_gui/pipeline.py
+++ plic_pipeline_gui/plic_pipeline_gui/pipeline.py
@@ -10,6 +10,7 @@ separation = [
         ['drop_empty_cols', "Drop empty columns"],
         ['visit_cols_to_rows', "Move different visits to one row each"],
         ['convert_string_values', "Convert string to numbers"],
+        ['export_mapped_columns', "Export encoded columns"],
         ['drop_useless_columns', "Remove unuseful columns for the AI"],
         ['fix_useful_string_columns', "Fix known problems with some columns"],
         ['translate_cols', "Translate column labels into English"]
@@ -28,6 +29,7 @@ def trigger(filename, study):
         imp.drop_empty_cols,
         imp.visit_cols_to_rows,
         imp.convert_string_values,
+        imp.export_mapped_columns,
         imp.drop_useless_columns,
         imp.fix_useful_string_columns,
         imp.translate_cols,
@@ -38,8 +40,16 @@ def trigger(filename, study):
     })
 
     for i in enumerate(functions):
+        status = {
+            "current_stage": i[1].__name__,
+            "next_stage": len(functions) > i[0]+1 and functions[i[0]+1].__name__ or None,
+        }
         try:
             i[1]()
+            if i[1].__name__ == "export_mapped_columns":
+                status["artifacts"] = {
+                    "Encoded columns association": imp.cols_export
+                }
         except Exception as e:
             print(e)
             yield "\n" + json.dumps({
@@ -48,10 +58,7 @@ def trigger(filename, study):
                 "error": "An error (%s) occurred while running %s" % (e, i[1].__name__)
             })
             raise StopIteration
-        yield "\n" + json.dumps({
-            "current_stage": i[1].__name__,
-            "next_stage": len(functions) > i[0]+1 and functions[i[0]+1].__name__ or None,
-        })
+        yield "\n" + json.dumps(status)
 
     yield "\n" + json.dumps({
         "status": "complete"
