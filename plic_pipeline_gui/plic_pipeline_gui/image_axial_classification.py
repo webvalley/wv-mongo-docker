0fb666f2c2ca12073829774eb30c3b67b22b3201
frikai <01modjul@rgtfo-me.it>
2019-07-03 17:00:33 +0200

    Added function to separate classified images into two different subfolders
    Signed-off-by: frikai <01modjul@rgtfo-me.it>

--- plic_pipeline_gui/plic_pipeline_gui/image_axial_classification.py
+++ plic_pipeline_gui/plic_pipeline_gui/image_axial_classification.py
@@ -7,9 +7,10 @@ import os
 
 class AxialClassifier:
 
-    def __init__(self, in_dir, out_dir = ""):
+    def __init__(self, in_dir, out_dir = False):
         self.files_in_dir = in_dir
         self.files_out_dir = out_dir
+        self.files_out_new_dirs = []
         self.file_list = os.listdir(in_dir)
         self.x = []
         self.model = None
@@ -40,9 +41,30 @@ class AxialClassifier:
         self.classified_list = list(zip(self.file_list, view_list, confidence_list))
         return self.classified_list
 
+    def move_files_to_new_folders(self):
+        if not self.files_out_dir:
+            self.files_out_dir = self.files_in_dir
+
+        new_folder_appendices = ["/sagittal", "/non_sagittal"]
+        self.files_out_new_dirs = [os.path.join(self.files_out_dir + x) for x in new_folder_appendices]
+
+        for nd in self.files_out_new_dirs:
+            new_folder = os.path.join(self.files_out_dir, nd)
+            if not os.path.exists(new_folder):
+                os.makedirs(new_folder)
+
+        for item in self.classified_list:
+            if item[1] == 1:
+                os.rename(os.path.join(self.files_in_dir, item[0]),
+                          os.path.join(self.files_out_new_dirs[0], item[0]))
+            else:
+                os.rename(os.path.join(self.files_in_dir, item[0]),
+                          os.path.join(self.files_out_new_dirs[1], item[0]))
+
 
 if __name__ == "__main__":
     classifier = AxialClassifier("C:/Users/julix/Documents/temp/chierici eco/anon/WV_PROVA_3")
     classifier.import_model("NN_model/model_even_better.h5")
     classifier.img_to_array_list()
     print(classifier.classify_axis())
+    classifier.move_files_to_new_folders()
