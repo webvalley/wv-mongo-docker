8c2e4c5a6a03ce679f03cc63f57667b66bab636a
Marco Marinello <marco.marinello@school.rainerum.it>
2019-07-04 11:49:59 +0200

    Start rewriting plots

--- plic_pipeline_gui/plic_pipeline_gui/somenzi_cazzo.py
+++ plic_pipeline_gui/plic_pipeline_gui/somenzi_cazzo.py
@@ -2,7 +2,9 @@
 # collection informations
 # Copyright (c) 2019 Paolo Somenzi <paolo.somenzi@school.rainerum.it>
 # Copyright (c) 2019 Marco Marinello <marco.marinello@school.rainerum.it>
+# Copyright (c) 2019 Stefano Fioravanzo <stefano.fioravanzo@gmail.com>
 
+import pandas as pd
 from bokeh import plotting, embed
 from bokeh.models import Range1d
 
@@ -22,18 +24,15 @@ units = {
 }
 
 
-def example_graph1(patients):
+def score_classes_plot(patients):
+    palette = ["#715F99", "#3867FF", "#FFCD78", "#CC7318"]
     cats = ["< 1", "1 < x < 2", "2 < x < 5", "> 5"]
-    vals = [0, 0, 0, 0]
-    for patient in patients:
-        if patient["score"] < 1:
-            vals[0] += 1
-        elif 1 <= patient["score"] < 2:
-            vals[1] += 1
-        elif 2 <= patient["score"] < 5:
-            vals[2] += 1
-        else:
-            vals[3] += 1
+    df = pd.DataFrame(patients)[["score"]]
+    df["colors"] = None
+    df.loc[df["score"]<1,["colors"]] = palette[0]
+    df.loc[(df["score"]<2) & (df["score"]>=1),["colors"]] = palette[1]
+    df.loc[(df["score"]<5) & (df["score"]>=2),["colors"]] = palette[1]
+    df.loc[5<df["score"],["colors"]] = palette[3]
     plot = plotting.figure(
         title="SCORE classes distribution",
         x_range=cats,
@@ -42,47 +41,13 @@ def example_graph1(patients):
         plot_width=400,
         plot_height=400
     )
-    plot.vbar(x=cats, top=vals, width=0.9)
+    plot.vbar(x=cats, top=df["score"], width=0.9)
     return plot
-
-def example_graph2(patients):
-    cats = ["gatti", "cani", "giraffe"]
-    vals = [100, 10, 60]
-    plot = plotting.figure(
-        title="Most liked animal",
-        x_range=cats,
-        toolbar_location=None,
-        tools="",
-        plot_width=400,
-        plot_height=400
-    )
-    plot.vbar(x=cats, top=vals, width=0.9)
-    return plot
-
-
-def plot_for_each_variable(patients):
-    vars = {
-        var: [] for var in patients[0] if var != "_id" and type(patients[0][var]) in (int, float)
-    }
-    for pat in patients:
-        for var in vars:
-            vars[var].append(pat[var])
-    plots = []
-    for var in vars:
-        plot = plotting.figure(
-            title=var,
-            plot_width=400,
-            plot_height=400
-        )
-        plot.circle(list(range(len(vars[var]))), vars[var], size=10, color="navy", alpha=0.5)
-        plots.append(plot)
-    return plots
 
 
 def collection_graphs(patients):
     f = [
-        example_graph1,
-        example_graph2
+        score_classes_plot,
     ]
     plots = [
         x(patients) for x in f
